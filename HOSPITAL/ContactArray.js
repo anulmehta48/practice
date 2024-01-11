72add523b820eb010647e4f2929fe127180549c1
anulmehta48 <anulmehta1848@gmail.com>
2024-01-11 12:07:40 +0530

    s

--- HOSPITAL/ContactArray.js
+++ HOSPITAL/ContactArray.js
@@ -6,7 +6,24 @@
 // let newFlateArray=newArray.flat(Infinity) //It will combiend all array into one single array
 // console.log(newFlateArray);
 
+// let arr=[2,3,4,[2,5,7],[2,3,4,5,6]]
+// let newArray=arr.flat(Infinity)
+// console.log(newArray);
 
-let arr=[2,3,4,[2,5,7],[2,3,4,5,6]]
-let newArray=arr.flat(Infinity)
-console.log(newArray);
+let arr = [2, 3, 4, [2, 5, 7], [2, 3, 4, 5, 6]];
+
+function CombiedSingleArry(arr) {
+  let newArray = [];
+  function flatedArray(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof(arr[i] )=== "object" && arr[i] !== null) {
+          flatedArray(arr[i])
+        }else{
+            newArray.push(arr[i])
+        }
+      }
+  }
+  flatedArray(arr)
+  return newArray
+}
+console.log(CombiedSingleArry(arr));
--- HOSPITAL/UpperCase.js
+++ HOSPITAL/UpperCase.js
@@ -11,6 +11,19 @@
 // console.log(MakeUppercase(sentense)) 
 
 
+
+// let string="my name is anul"
+// function CapitalEachSecond(string){
+//   let word=string.split(" ")
+//   console.log(word)
+//   for(let i=0;i<word.length;i++){
+//     word[i]=word[i].charAt(0)+ word[i].charAt(1).toUpperCase()+word[i].slice(2)
+//   }
+//   return word.join(" ")
+// }
+// console.log(CapitalEachFirst(string))
+
+
 //Change all uppercase letter to lowercase and lowercase to uppercase
 let string="The brOWN  FoX"
 function ChnageLetters(string){
