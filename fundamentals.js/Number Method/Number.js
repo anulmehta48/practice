3aaec04b80bcc3247a510c7d75b52909be93cd4c
anulmehta48 <anulmehta1848@gmail.com>
2024-03-24 08:00:02 +0530

    s

---
+++ fundamentals.js/Number Method/Number.js
@@ -0,0 +1,31 @@
+// let myNum=500.5445
+// console.log(myNum);
+
+
+// let myNum2=new Number(5000000)
+// console.log(myNum2);
+
+// console.log(myNum.toString());
+// console.log(myNum.toFixed(2));
+// console.log(myNum.toPrecision(3));
+// console.log(myNum2.toLocaleString('en-IN'));
+
+console.log(Math);
+console.log(Math.abs(45.0));
+console.log(Math.round(45.55));
+console.log(Math.ceil(5.2));
+console.log(Math.floor(5.2));
+console.log(Math.sqrt(625));
+console.log(Math.min(4,5,2,9,41,5,3));
+console.log(Math.max(4,5,2,9,41,5,3));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+let min=10;
+let max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
